e7857351b6779b96df25f07afa313c68fabfe66b
Boris Reuderink <boris@cortext.nl>
2023-05-28 17:59:10 +0200

    Convert to package.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simpleattention",
+    version="1.0.0",
+    author="Boris Reuderink",
+    author_email="boris@cortext.nl",
+    description="Simple attention modules for PyTorch.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/breuderink/simpleattention",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
